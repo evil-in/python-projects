74e769e5f09f69ae44614ab1cd11641b8a9a3672
evil-in <57084343+evil-in@users.noreply.github.com>
2020-07-09 16:13:26 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,32 @@
+import re
+
+heading="A Whiz Calculator"
+print(heading)
+print("Type quit to exit\n")
+
+previous = 0
+run = True
+
+def performMath():
+    global run
+    global previous
+    if previous == 0:
+        equation = input("Enter equation:")
+    else:
+       equation=input(str(previous))
+
+    if equation == "quit":
+        print("Adios, amigo")
+        run = False
+    else:
+
+        equation = re.sub('[a-zA-Z,.:()" "]','',equation)
+        if previous == 0:
+             previous = eval(equation)
+        else:
+            previous= eval(str(previous) + equation)
+
+
+
+while run:
+    performMath()
